00be5d6f81236b315d89b44b507d23dce7320917
Markus2409 <marco.cuscuna@studio.unibo.it>
2025-05-02 16:45:05 +0200

    script_recover_representative_kunitz modified comments!

--- script_recover_representative_kunitz.sh
+++ script_recover_representative_kunitz.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/bash
-cat rcsb_pdb_custom_report_20250410062557.csv |tr -d '"' | awk -F ',' '{if (length($2)>0) {name=$2}; print name ,$3,$4,$5}' | grep PF00014| awk '{print">"$1"_"$3;print$2}' > pdb_kunitz.fasta #a partire dal file pdb report ti scarica le sequenze dei domini pf00014 di tutte le proteine. 
-cd-hit -i pdb_kunitz.fasta -o pdb_kunitz.clstr -c 0.9 #questo ti clusterizza in base alla similarità. -c 0.9 hanno una similarita di 0.9
-clstr2txt.pl pdb_kunitz.clstr.clstr | awk '{if ($5==1) print $1}' > pdb_kunitz_rp.ids #questo ti prende le piu rappresentative per ciascun cluster e te le mette in un file a parte
 
+# Extract sequences of PF00014 domains from the PDB custom report
+cat rcsb_pdb_custom_report_20250410062557.csv | tr -d '"' \
+| awk -F ',' '{if (length($2)>0) {name=$2}; print name ,$3,$4,$5}' \
+| grep PF00014 \
+| awk '{print ">"$1"_"$3; print $2}' > pdb_kunitz.fasta
+
+# Cluster the sequences using CD-HIT at 90% identity threshold
+cd-hit -i pdb_kunitz.fasta -o pdb_kunitz.clstr -c 0.9
+
+# Extract the most representative ID from each cluster
+clstr2txt.pl pdb_kunitz.clstr.clstr | awk '{if ($5==1) print $1}' > pdb_kunitz_rp.ids
+
+# Retrieve the sequences of the representative IDs and store them in a new FASTA file
 for i in `cat pdb_kunitz_rp.ids`; do
   grep -A 1 "^>$i" pdb_kunitz.fasta | tail -n 2 >> pdb_kunitz_rp.fasta
-done 
-# with this command i create a file that contain the sequences of ids representative that we got from cd-hit
-#at this point you have to check if there are proteins that has sequences too long. In this case delete it. otherwise launch pdbefold!
-grep ">" pdb_kunitz_rp.fasta |tr -d ">" | tr "_" ":" > pdb_efold_ids.txt #generate the ids that you have to insert in pdbefold to do the msa of structure
+done
+
+# This file now contains only the representative sequences selected from CD-HIT
+
+# OPTIONAL: check manually for sequences that are too long and remove them before continuing
+
+# Generate the PDBefold input format (convert IDs to the expected format: PDB:CHAIN)
+grep ">" pdb_kunitz_rp.fasta | tr -d ">" | tr "_" ":" > pdb_efold_ids.txt
